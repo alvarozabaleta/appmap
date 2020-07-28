33dc55c96996ec1ad98cce09e328b86924bbd128
alvarozabaleta <azabaletao1900@alumno.ipn.mx>
2020-07-28 15:56:55 -0500

    Updates

---
+++ precache-manifest.cfde646d2d85fb82cf40a83d65525d0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af460a76856c9fd68df3a5624c5d730c",
+    "url": "/appmap/index.html"
+  },
+  {
+    "revision": "e28d1ba2abbcda6afa98",
+    "url": "/appmap/static/css/2.8d863f11.chunk.css"
+  },
+  {
+    "revision": "ad9e41c52c5bd922ceac",
+    "url": "/appmap/static/css/main.0fed2176.chunk.css"
+  },
+  {
+    "revision": "e28d1ba2abbcda6afa98",
+    "url": "/appmap/static/js/2.57cee529.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/appmap/static/js/2.57cee529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9e41c52c5bd922ceac",
+    "url": "/appmap/static/js/main.16ccbd90.chunk.js"
+  },
+  {
+    "revision": "bb5c5a0651c3b84d0a84",
+    "url": "/appmap/static/js/runtime-main.07b69f39.js"
+  }
+]);
